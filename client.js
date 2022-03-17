bced822edaf73b198d57cafed2681b2a42d75c0b
Christopher Ortega <A01634500@itesm.mx>
2022-03-16 20:35:06 -0600

    C2S2C comms, obtaining conn users, sending message to server

--- client.js
+++ client.js
@@ -1,5 +1,71 @@
+
+const random = require("random");
 const
     io = require("socket.io-client"),
     ioClient = io.connect("http://localhost:8000");
 
-ioClient.on("seq-num", (msg) => console.info(msg));
+//ioClient.emit("username", "Chris");
+
+// * Global server variable
+var connUsers = {};
+
+var toSend = "";
+
+ioClient.on("message", (data) => {
+    console.info(data)
+})
+
+ioClient.on("alert", (data) => {
+    console.warn(data)
+});
+
+ioClient.on("from", (data) => {
+    console.log(data.from + " > Me : " + data.message);
+})
+
+ioClient.on("username", (username) => console.log("Successfully logged in as: " + username))
+
+ioClient.on("connUsers", (data) => {
+    //console.info(`Currently connected users [${data.num_users}]:`);
+    connUsers = data.users;
+
+    if (data.users.length == 0) {
+        return;
+    }
+    // * Once connUsers is populated
+    data.users.forEach(user => {
+        console.log(`Me > ${user.username}: `)
+        console.log(toSend);
+        ioClient.emit("to", {
+            "to": user.socketId,
+            "message": toSend
+        });
+    }); 
+
+});
+
+
+let sendMessageToServer = (message) => {
+    console.log("Me > Server:");
+    console.log(message);
+    ioClient.emit("msg", message);
+}
+
+let sendMessage = (message) => {
+    toSend = message;
+    ioClient.emit("connUsers");
+}
+
+// * Testing communication temporal method
+setInterval(() => {
+    if (random.boolean()) {
+        sendMessageToServer({
+            "zombies": 100,
+            "survivors": 3,
+            "message": "Ayudenos que petateamos."
+        });
+    } else {
+        sendMessage("Que show bro, rolen comida");
+    }
+    
+}, 1000 * random.int(1, 10))
--- server.js
+++ server.js
@@ -2,26 +2,75 @@ const
     {Server} = require("socket.io"),
     server = new Server(8000);
 
-let
-    sequenceNumberByClient = new Map();
+// * Map containing the currently connected clients
+let clients = new Map();
+let hist_clients = new Map();
 
-// event fired every time a new client connects:
+
 server.on("connection", (socket) => {
     console.info(`Client connected [id=${socket.id}]`);
-    // initialize this client's sequence number
-    sequenceNumberByClient.set(socket, 1);
+    // * save client to directory
+    clients.set(socket.id, {
+        "socket": socket,
+        "username": "User"+(clients.size+1)
+    });
+    
+    // * Save historic clients
+    if (!hist_clients.has(socket.id)) {
+        hist_clients.set(socket.id, { "lastConn": new Date().toISOString() });
+    }
 
-    // when socket disconnects, remove it from the list:
+    // * when socket disconnects, remove it from the list, update hist_clients:
     socket.on("disconnect", () => {
-        sequenceNumberByClient.delete(socket);
+        clients.delete(socket.id);
+        hist_clients.set(socket.id, { "lastConn": new Date().toISOString() })
         console.info(`Client gone [id=${socket.id}]`);
     });
+
+    socket.on("username", (username) => {
+        clients.set(socket.id, {
+            "socket": socket,
+            "username": username
+        })
+        socket.emit("username", username)
+    });
+
+    socket.on("msg", (data) => {
+        console.info(clients.get(socket.id).username + ">");
+        console.info(data);
+    });
+
+    socket.on("to", (data) => {
+        // * Check if client exists
+        if (clients.has(data.to)) {
+            clients.get(data.to).socket.emit("from", {"from": clients.get(socket.id).username , "message": data.message});
+        } else {
+            socket.emit("error", "There is no connected client with that id: "+data.to);
+        }
+    });
+
+    socket.on("connUsers", (data) => {
+        console.log(clients.get(socket.id).username + " requested for connected users...");
+
+        let connUsers = []; 
+        for (let [socketId, socketData] of clients.entries()) {
+            if (socket.id !== socketId) {
+                connUsers.push({
+                    "username": clients.get(socketId).username,
+                    "socketId": socketId
+                })
+            }
+        }
+
+        socket.emit("connUsers", {
+            "num_users": clients.size,
+            "users": connUsers
+        });
+    })
 });
 
-// sends each client its current sequence number
-setInterval(() => {
-    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-        client.emit("seq-num", sequenceNumber);
-        sequenceNumberByClient.set(client, sequenceNumber + 1);
-    }
-}, 1000);
+/*
+    * msg - Messaging Client2Server
+    * msg - Messaging Server2Client
+    * to  - Messaging Client2Server2Client
+ */
